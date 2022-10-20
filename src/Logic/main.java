a4ddf5f1aaa4982d046749fa40dce4f993793ef6
Asthira <sketchjarablanco@gmail.com>
2022-10-20 12:54:45 -0300

    register panel

---
+++ src/Logic/main.java
@@ -0,0 +1,14 @@
+package Logic;
+import javax.swing.SwingUtilities;
+
+import Panels.*;
+
+public class main {
+
+	public static void main(String[] args) {
+		new RegisterPanel().setVisible(true);
+
+	}
+
+}
+
