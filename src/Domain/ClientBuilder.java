5071fd17a356063948671f23836f2ee925537709
claudeus123 <claudio.pizarro@alumnos.ucn.cl>
2022-10-27 16:02:13 -0300

    solved conflicts

--- src/Domain/ClientBuilder.java
+++ src/Domain/ClientBuilder.java
@@ -6,37 +6,45 @@ public class ClientBuilder implements IUserBuilder{
     private String password;
     private int number;
     private String mail;
+    private Transactions transactions;
 
     // + LISTAS
     @Override
     public void setRut(String rut) {
         // TODO Auto-generated method stub
-    	this.rut = rut;
+        this.rut = rut;
     }
 
     @Override
     public void setName(String name) {
         // TODO Auto-generated method stub
-    	this.name = name;
+        this.name = name;
     }
 
     @Override
     public void setPassword(String password) {
         // TODO Auto-generated method stub
-    	this.password = password;
-    }
-    
-    public void setNumber(int number) {
-        // TODO Auto-generated method stub
-    	this.number = number;
+        this.password = password;
     }
 
     @Override
-    public void setMail(String string) {
+    public void setMail(String mail) {
         // TODO Auto-generated method stub
-    	this.rut = rut;
+        this.mail = mail;
     }
 
+    public void setNumber(int number){
+        this.number = number;
+    }
+    
+    public void setTransactions(Transactions transactions){
+        this.transactions = transactions;
+    }
+
+    public Client getResult(String rut, String name, String password, String mail, int number, Transactions transactions){
+        return new Client (rut, name, password, mail, number, transactions);
+    }
     //setNumber
     //get --> llama al constructor
+    //new Client(x,y,z,a,b,c,d,e);
 }
