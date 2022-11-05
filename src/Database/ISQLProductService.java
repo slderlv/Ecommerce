b2781a409f0fb7c8177ec7e580701eacc0097d19
claudeus123 <claudio.pizarro@alumnos.ucn.cl>
2022-11-04 22:43:48 -0300

    create-read of SQLProductService impl

--- src/Database/ISQLProductService.java
+++ src/Database/ISQLProductService.java
@@ -1,13 +1,14 @@
 package Database;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 
 import Domain.Product;
 
-public class ISQLProductService {
+public class ISQLProductService implements ISQLCreate<String> , ISQLUpdate<Product>, ISQLDelete<Product>, ISQLRead<String>{
 
 	public void create(String category) {
 		// TODO Auto-generated method stub
@@ -18,6 +19,43 @@ public class ISQLProductService {
         }catch (SQLException e){
             JOptionPane.showMessageDialog(null, "Error con la consulta" + e);
         }
+	}
+
+	@Override
+	public ResultSet read(String t) {
+		try{  
+            PreparedStatement statement = SQLConnection.getSQLConnection().connect().prepareStatement("SELECT * FROM products WHERE category=?");
+            statement.setString(1,t);
+            
+            ResultSet response = statement.executeQuery();
+            return response;
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error con la consulta" + e);
+        }
+		return null;
+	}
+	
+	public ResultSet read() {
+		try{  
+            PreparedStatement statement = SQLConnection.getSQLConnection().connect().prepareStatement("SELECT * FROM products WHERE category=?");
+            ResultSet response = statement.executeQuery();
+            return response;
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error con la consulta" + e);
+        }
+        return null;
+	}
+
+	@Override
+	public void delete(Product t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Product t) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
