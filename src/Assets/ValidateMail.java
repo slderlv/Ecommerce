3c2d02ec01c64d38fd413a2ca7782c60b140a5ae
Vicente <vicente.briceno@alumnos.ucn.cl>
2022-11-08 19:05:13 -0300

    client frame updated

--- src/Assets/ValidateMail.java
+++ src/Assets/ValidateMail.java
@@ -3,7 +3,7 @@ package Assets;
 import java.util.regex.Pattern;
 
 public class ValidateMail {
-	static boolean isValid(String mail) {
+	public static boolean isValid(String mail) {
 		return Pattern.matches("^[a-z0-9]+(?:.[a-z0-9]+){0,5}@[a-z0-9]+(?:.[a-z0-9]{2,15}){1,5}" , mail);
 	}
 }
