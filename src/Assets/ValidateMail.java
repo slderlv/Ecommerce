cf9dfc0b2fc36bbe095605f7a03479a9a86584cb
claudeus123 <claudio.pizarro@alumnos.ucn.cl>
2022-11-08 13:14:41 -0300

    validateMail changes

--- src/Assets/ValidateMail.java
+++ src/Assets/ValidateMail.java
@@ -4,6 +4,6 @@ import java.util.regex.Pattern;
 
 public class ValidateMail {
 	static boolean isValid(String mail) {
-		return Pattern.matches("^[a-z0-9]+(?:.[a-z0-9]+){0,5}@[a-z0-9]+(?:.[a-z0-9]+){1,5}" , mail);
+		return Pattern.matches("^[a-z0-9]+(?:.[a-z0-9]+){0,5}@[a-z0-9]+(?:.[a-z0-9]{2,15}){1,5}" , mail);
 	}
 }
