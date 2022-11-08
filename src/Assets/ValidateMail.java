e7a92af7407dabfe47b6a57db5f3b7c45ffea1d2
claudeus123 <claudio.pizarro@alumnos.ucn.cl>
2022-11-08 11:39:58 -0300

    function parameter changed

--- src/Assets/ValidateMail.java
+++ src/Assets/ValidateMail.java
@@ -3,7 +3,7 @@ package Assets;
 import java.util.regex.Pattern;
 
 public class ValidateMail {
-	static boolean isValid(String rut) {
-		return Pattern.matches("^[a-z0-9]+(?:.[a-z0-9]+){0,5}@[a-z0-9]+(?:.[a-z0-9]+){1,5}" , rut);
+	static boolean isValid(String mail) {
+		return Pattern.matches("^[a-z0-9]+(?:.[a-z0-9]+){0,5}@[a-z0-9]+(?:.[a-z0-9]+){1,5}" , mail);
 	}
 }
